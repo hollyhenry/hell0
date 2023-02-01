e4401a124acfc6893bd7a1caf5aea3e98eb7bff8
hollyhenry <110806825+hollyhenry@users.noreply.github.com>
2023-01-31 20:25:11 -0600

    intitial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Holly Henry
+ *         Hollylhenry@gmail.com
+ * Date: 2023/01/x31
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Holly Henry\n");
+
+  return 0;
+}
